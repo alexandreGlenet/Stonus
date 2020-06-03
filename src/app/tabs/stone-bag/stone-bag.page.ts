5359497f33575d81a271f13eb90b9e8f190cf180
Alexandre Glenet <alexandreglenet30@gmail.com>
2020-06-03 16:52:24 +0200

    amélio

--- src/app/tabs/stone-bag/stone-bag.page.ts
+++ src/app/tabs/stone-bag/stone-bag.page.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SegmentChangeEventDetail } from "@ionic/core";
+import { ApiService } from "src/app/services/api.service";
+import { LoadingController } from "@ionic/angular";
+
+import * as L from "leaflet";
+//import { antPath } from "leaflet-ant-path";
+import "leaflet/dist/images/marker-shadow.png";
+import "leaflet/dist/images/marker-icon-2x.png";
 
 @Component({
-  selector: 'app-stone-bag',
-  templateUrl: './stone-bag.page.html',
-  styleUrls: ['./stone-bag.page.scss'],
+	selector: "app-stone-bag",
+	templateUrl: "./stone-bag.page.html",
+	styleUrls: ["./stone-bag.page.scss"],
 })
 export class StoneBagPage implements OnInit {
+	//Map
+	map: L.Map;
+	newMarker: any;
+	address: string[];
 
-  constructor() { }
+	constructor(
+		private api: ApiService,
+		private loadingCtrl: LoadingController
+	) {}
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
 
+	ionViewWillEnter() {
+		console.log("willEnter");
+	}
+
+	ionViewDidEnter() {
+		console.log("didEnter");
+		console.log(this.map);
+	}
+
+	ionViewWillLeave() {
+		console.log("willleave");
+		//console.log(this.map);
+	}
+
+	// ionViewDidLeave() {
+	// 	this.map.remove();
+	// }
 }
--- src/app/tabs/stone/stone.page.ts
+++ src/app/tabs/stone/stone.page.ts
@@ -4,7 +4,7 @@ import { ApiService } from "src/app/services/api.service";
 import { LoadingController } from "@ionic/angular";
 
 import * as L from "leaflet";
-import { antPath } from "leaflet-ant-path";
+//import { antPath } from "leaflet-ant-path";
 import "leaflet/dist/images/marker-shadow.png";
 import "leaflet/dist/images/marker-icon-2x.png";
 
@@ -33,15 +33,16 @@ export class StonePage implements OnInit {
 	});
 
 	mainLayer = L.tileLayer(
+		"https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png",
 		//http://tile.mtbmap.cz/mtbmap_tiles/{z}/{x}/{y}.png
 		//https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png
-		"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+		//"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
 		{
 			attribution:
-				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+				'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY- SA</a>',
 			minZoom: 4,
-			maxZoom: 16,
-			tileSize: 256,
+			maxZoom: 18,
+			//tileSize: 512,
 			//zoomOffset: -1,
 		}
 	);
@@ -88,12 +89,11 @@ export class StonePage implements OnInit {
 		);
 	}
 
-	loadMap() {
-		this.map = new L.Map("mapId2").setView([50.64, 5.576], 19); // fitworld fait buguer donc j'assigne une coordonnée au chargmenet ici l'univ de liege
-		//this.locatePosition();
-		this.mainLayer.addTo(this.map);
-		//this.map.on("locationfound", this.onLocationFound);
-	}
+	// loadMap() {
+	// 	this.map = new L.Map("mapId2").setView([50.64, 5.576], 19); // fitworld fait buguer donc j'assigne une coordonnée au chargmenet ici l'univ de liege
+	// 	//this.locatePosition();
+	// 	this.mainLayer.addTo(this.map);
+	// }
 
 	locatePosition() {
 		this.map
@@ -105,18 +105,24 @@ export class StonePage implements OnInit {
 				enableHighAccuracy: true,
 			})
 			.on("locationfound", (e: any) => {
-				var radius = e.accuracy;
+				var radius = e.accuracy / 2;
 				this.newMarker = L.marker([e.latitude, e.longitude], {
 					draggable: false,
 				}).addTo(this.map);
 
-				this.newMarker.bindPopup("Je suis ici !!!").openPopup();
+				this.newMarker
+					.bindPopup("Je suis à !!!" + radius + " metres de ce point")
+					.openPopup();
 
 				L.circle([e.latitude, e.longitude], radius).addTo(this.map);
 
 				this.newMarker.on("dragend", () => {
 					const position = this.newMarker.getLatLng();
 				});
+			})
+			.on("locationError", (e: any) => {
+				alert(e.message);
+				console.log(e.message);
 			});
 	}
 
@@ -126,64 +132,35 @@ export class StonePage implements OnInit {
 		this.locatePosition();
 	}
 
-	// locate() {
-	// 	this.map.locate({
-	// 		setView: true,
-	// 		maxZoom: 6,
-	// 		watch: true,
-	// 		enableHighAccuracy: true,
-	// 	});
-	// }
-
-	// onLocationFound(e) {
-	// 	var radius = e.accuracy;
-
-	// 	L.marker(e.latlng)
-	// 		.addTo(this.map)
-	// 		.bindPopup("You are within " + " meters from this point")
-	// 		.openPopup();
-
-	// 	L.circle(e.latlng).addTo(this.map);
-	// }
-
-	// onLocationError(e) {
-	//   alert(e.message);
-	// }
-
 	ionViewWillEnter() {
-		console.log("willEnter");
+		console.log("willEnter-stone.page");
 	}
 
 	ionViewDidEnter() {
-		console.log("didEnter");
+		console.log("didEnter-stone.page");
 		this.loadLocateMap();
-
-		// raccourci très pratique pour zoomer la vue de la carte à l'emplacement détecté
-		//this.locatePosition();
-
-		//this.map.on("locationerror", this.onLocationError);
-
-		//console.log(this.locate)
-		//this.map.setView([0,0],0);
+		console.log(this.map);
 	}
 
-	ionViewWillLeave(event: CustomEvent<SegmentChangeEventDetail>) {
-		console.log("willleave");
+	ionViewWillLeave() {
+		console.log("willleave-stone.page");
 		//console.log(this.map);
 		if (this.map !== undefined) {
 			this.map.remove();
+			//this.map = null;
 			//console.log(this.map);
+		} else {
+			//this.map.remove();
 		}
 	}
 
+	// ionViewDidLeave() {
+	// 	this.map.remove();
+	// }
+
+	// ION-SEGMENT (SWITCH)
+
 	onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
 		//console.log(event.detail);
-		if (event.detail.value === "map") {
-			//this.map.remove();
-			//this.loadMap();
-			this.loadLocateMap();
-		} else if (event.detail.value === "stones-list") {
-			this.map.remove();
-		}
 	}
 }
