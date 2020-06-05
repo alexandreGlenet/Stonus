9146412f54a5d48733c9ed7204e3153f2bac093d
Alexandre Glenet <alexandreglenet30@gmail.com>
2020-06-05 14:48:37 +0200

    Ok Guards CanLoad in tabs-routing.
    dans app ca bug du au routing, donc en le faisant dans tabs je n'ai aucun probleme et les redirections sont bonnes.

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,39 +1,45 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { TabsPage } from './tabs.page';
+import { TabsPage } from "./tabs.page";
+import { AuthentifictionGuard } from "../authentification/authentifiction.guard";
 
 const routes: Routes = [
-  {
-    path: 'tabs',
-    component: TabsPage,
-    children: [
-
-      {
-        path: 'stone',
-        loadChildren: () => import('./stone/stone.module').then(m => m.StonePageModule)
-      }, 
-      {
-        path: 'stone-bag',
-        loadChildren: () => import('./stone-bag/stone-bag.module').then(m => m.StoneBagPageModule)
-      },
-      {
-        path: 'profile',
-        loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
-      },
-      {
-        path: '', // redirection pour les cas ou places/tabs
-        redirectTo: 'tabs/stone',
-        pathMatch: 'full'
-      }
-    ]
-  },
-  
-  
+	{
+		path: "tabs",
+		component: TabsPage,
+		children: [
+			{
+				path: "stone",
+				loadChildren: () =>
+					import("./stone/stone.module").then((m) => m.StonePageModule),
+				canLoad: [AuthentifictionGuard],
+			},
+			{
+				path: "stone-bag",
+				loadChildren: () =>
+					import("./stone-bag/stone-bag.module").then(
+						(m) => m.StoneBagPageModule
+					),
+				canLoad: [AuthentifictionGuard],
+			},
+			{
+				path: "profile",
+				loadChildren: () =>
+					import("./profile/profile.module").then((m) => m.ProfilePageModule),
+				canLoad: [AuthentifictionGuard],
+			},
+			{
+				path: "", // redirection pour les cas ou places/tabs
+				redirectTo: "tabs/stone",
+				pathMatch: "full",
+			},
+		],
+	},
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule],
 })
 export class TabsPageRoutingModule {}
